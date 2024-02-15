d009ff4de152ad0c0e71db7348a6614e8a3ced1e
lucasion22 <lucas.costa@ionsistemas.com.br>
2024-02-15 09:59:02 -0300

    Melhoria01 - Projeto iniciado com Spring Initi

---
+++ src/main/java/com/servicos/lanchedogordo/LanchedogordoApplication.java
@@ -0,0 +1,26 @@
+package com.servicos.lanchedogordo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class LanchedogordoApplication {
+
+	private String nomeInicial;
+
+	@GetMapping("/hellow")
+	public String iniciandoAplicacao(){
+		nomeInicial = "===========================================" +
+				"Incializando aplicação Spring Boot." +
+				"==========================================";
+		return nomeInicial;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanchedogordoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servicos/lanchedogordo/LanchedogordoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicos.lanchedogordo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanchedogordoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
